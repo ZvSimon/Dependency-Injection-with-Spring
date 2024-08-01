aace2b7e7c4d79b2ecfd9919da2ffd737e8039e9
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-08-01 18:29:10 +0200

    Initial commit

---
+++ src/main/java/guru/springframework/dependencyinjectionwithspring/DependencyInjectionWithSpringApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.dependencyinjectionwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInjectionWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DependencyInjectionWithSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/dependencyinjectionwithspring/DependencyInjectionWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.dependencyinjectionwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
